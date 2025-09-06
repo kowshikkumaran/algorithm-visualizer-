0cbb2e07cafc7b5cb9ba46e03e4ea44a4eb706a1
kowshik kumaran <kowshikkumaran.s12345@gmail.com>
2025-09-06 22:40:28 +0530

    java fix

--- src/main.java
+++ src/main.java
@@ -1,5 +1,23 @@
-public class main {
-  public static void main(String args[]) {
-    System.out.print("algo visualizer babe");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    Label helloLabel = new Label("Hello, JavaFX!");
+    StackPane root = new StackPane(helloLabel);
+    Scene scene = new Scene(root, 400, 300);
+
+    primaryStage.setTitle("Algorithm Visualizer");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
   }
 }
