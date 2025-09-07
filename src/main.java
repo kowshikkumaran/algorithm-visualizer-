ae410fd111095c0d5ede7b8e1aca0a3be34b4fac
kowshik kumaran <kowshikkumaran.s12345@gmail.com>
2025-09-07 23:08:24 +0530

    javaFX

--- src/main.java
+++ src/main.java
@@ -1,38 +1,21 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-  @Override
-  public void start(Stage primaryStage) {
-    Label helloLabel = new Label("Hello, JavaFX!");
-    StackPane root = new StackPane(helloLabel);
-    Scene scene = new Scene(root, 400, 300);
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello JavaFX!");
+        Scene scene = new Scene(label, 400, 300);
 
-    primaryStage.setTitle("Algorithm Visualizer");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+        primaryStage.setTitle("Algorithm Visualizer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-  public static void main(String[] args) {
-    launch(args);
-  }
-
-  @Override
-  public void start(Stage primaryStage) {
-    Label helloLabel = new Label("Hello, JavaFX!");
-    StackPane root = new StackPane(helloLabel);
-    Scene scene = new Scene(root, 400, 300);
-
-    primaryStage.setTitle("Algorithm Visualizer");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
-
-  public static void main(String[] args) {
-    launch(args);
-  }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
